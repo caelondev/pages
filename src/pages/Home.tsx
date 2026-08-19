8dc1995f960278bce492c30e372fc49caf0291e9
caelondev <git@caelondev.net>
2026-08-19 18:00:35 +0800

    feat(home): revamped the homepage and made the content my programming journey :3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,150 @@
-import BlogPost from "../components/BlogPost/BlogPost";
-import { Footer } from "../components/Footer/Footer";
-import { Hero } from "../components/Hero/Hero";
 import { LinkInfo } from "../components/LinkInfo/LinkInfo";
-import { Webring } from "../components/Webring/Webring";
-import { Webrings } from "../components/Webrings/Webrings";
-import "./Home.module.css";
+import styles from "./Home.module.css";
 
 export function Home() {
-  const links = [
-    { to: "mailto:me@caelondev.net", name: "email" },
-    { to: "https://blog.caelondev.net/", name: "blog" },
-    { to: "https://git.caelondev.net/", name: "codeberg" },
-  ];
-
   return (
     <main>
-      <div className="content">
-        <Hero />
-        <Webrings>
-          <Webring
-            name="stabring"
-            link="https://ring.stabbed.me"
-            next="https://ring.stabbed.me/next/from/caelondev.is-a.dev"
-            prev="https://ring.stabbed.me/prev/from/caelondev.is-a.dev"
-          />
-        </Webrings>
-        <BlogPost />
-        {links.map((link, index) => (
-          <LinkInfo
-            key={link.name}
-            to={link.to}
-            name={link.name}
-            index={index}
-          />
-        ))}
+      <div className={styles.parent}>
+        <h1 className={styles.title}>caelon</h1>
+        <p className={styles.subtitle}>systems programmer</p>
+        <hr />
+        <div className={styles.links}>
+          <LinkInfo to="mailto:me@caelondev.net" name="email" />
+          <LinkInfo to="https://blog.caelondev.net/" name="blog" />
+          <LinkInfo to="https://git.caelondev.net/" name="codeberg" />
+        </div>
+
+        <article className={styles.story}>
+          <p>hello. i'm Jericho. also known as caelon.</p>
+
+          <p>my "programming" journey started when i was 10.</p>
+
+          <p>
+            i LOVE playing minecraft. during break time, when i'm bored, pretty
+            much anytime.
+          </p>
+
+          <p>
+            one day, i was watching a youtube video about some clickbait-y
+            "summoning herobrine" shenanigans. while i was watching, i noticed
+            them holding a command block.
+          </p>
+
+          <p>as i looked at it, it felt... weird.</p>
+
+          <p>
+            i played minecraft all day, yet somehow, this was my first time
+            seeing this block. so, into minecraft i went. i typed
+          </p>
+
+          <code className={styles.command}>/give @s command_block</code>
+
+          <p>and started tinkering with it.</p>
+
+          <p>
+            it felt awesome. the command block was following the commands i gave
+            it, as if it was a tiny computer that actually listened to me...
+          </p>
+
+          <p>fast forward a few years... i was 13.</p>
+
+          <p>
+            it was vacation, right after our school's christmas party. i was
+            watching a load of youtube videos when i suddenly thought to myself:
+          </p>
+
+          <blockquote className={styles.quote}>
+            oh no, what if after this vacation, the teachers ask us what we did
+            during vacation? what am i going to tell them??
+          </blockquote>
+
+          <p>and then this weird idea came into my mind...</p>
+
+          <blockquote className={styles.quote}>
+            ah! that's it! i'm going to learn how to code!
+          </blockquote>
+
+          <p>
+            i hopped on youtube, watched a lot of videos, and the rest was
+            history...
+          </p>
+
+          <p>i learned EVERYTHING.</p>
+
+          <p>
+            i started with python, then got absolutely bamboozled with
+            information and burned out pretty quickly because i couldn't really
+            see my progress.
+          </p>
+
+          <p>so i moved on to web development.</p>
+
+          <p>
+            everything went smoothly. i was making websites, learning how
+            browsers worked, and generally having a good time.
+          </p>
+
+          <p>but eventually, i started feeling trapped.</p>
+
+          <p>
+            the browser felt like it was holding my hand. everything i wanted to
+            make had to go through this giant sandbox, and it felt like i
+            couldn't really make anything without the browser getting involved
+            somehow.
+          </p>
+
+          <p>so i tried making bots.</p>
+
+          <p>discord bots, specifically.</p>
+
+          <p>
+            and honestly, it felt nice. i made a bot with a bunch of
+            functionalities, and for the first time, i felt like i was making
+            something that actually did stuff on its own.
+          </p>
+
+          <p>
+            but then, a few months later, a guy named "Core Dumped" appeared on
+            my recommendations page on youtube.
+          </p>
+
+          <p>and i was like:</p>
+
+          <blockquote className={styles.quote}>
+            woah, that's cool! that's how a CPU works???
+          </blockquote>
+
+          <p>
+            i got addicted to his channel. to the point where i'd watch the same
+            videos over and over again, simply because of how amazed i was by
+            what was happening underneath all the abstractions.
+          </p>
+
+          <p>then an idea came into my mind:</p>
+
+          <blockquote className={styles.quote}>
+            i wanna learn systems programming.
+          </blockquote>
+
+          <p>and boom.</p>
+
+          <p>here i am right now.</p>
+
+          <p>the caelon everyone knows.</p>
+
+          <p>
+            the first language i started with in systems programming was Rust.
+            yes, i know. pretty brutal choice.
+          </p>
+
+          <p>but eventually, i also got into languages like Go and Zig.</p>
+
+          <p>
+            and somehow, that kid who just wanted to mess around with a weird
+            block in minecraft ended up here.
+          </p>
+        </article>
       </div>
-      <Footer />
     </main>
   );
 }
